fc9b4763bfaaac1a8b79afdb7be7c40695fd2397
Sarah Mukuti <cesarwrites24@gmail.com>
2022-08-11 23:22:34 +0300

    commit tree 08b84165b5e3db60d74e0f6d4c2447fa5388267b

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+badge_maker("Arel")
+
+def batch_badge_creator (arr)
+    do ||
+    "Hello, my name is #{name}."
+
+def assign_rooms(names)
+    rooms = 8
+    names.include?
+    while room <rooms 
+        "Hello #{name}! You'll be assigned to room #{room}!,"
+        room -=1 
+    end
+end
+assign_rooms(["Ariel", "Carol"])
